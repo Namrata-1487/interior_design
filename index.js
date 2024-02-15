3c894acc3249a06069b3a0c0e45c2d7c6d09b89e
namrata shirsat <namrata.shirsaat@gmail.com>
2024-02-15 20:15:14 +0530

    javascript added

--- index.js
+++ index.js
@@ -1,19 +1,8 @@
-// document.addEventListener("scroll", () => {
-//     const header = document.querySelector('header');
-//     if (window.scrollY > 0) {
-//         header.classList.add("scrolled");
-//     }
-//     else {
-//         header.classList.remove("scrolled");
-//     }
-
-// })
-let navbar = document.getElementsByClassName("navbar");
-let x = 0;
-function backgroundcolor() {
-  if (x > 0) {
-    navbar.style.backgroundColor = "black";
+document.addEventListener("scroll", () => {
+  const navE = document.querySelector(".navbar");
+  if (window.scrollY > 0) {
+    navE.classList.add("scrolled");
   } else {
-    navbar.style.backgroundColor = "white";
+    navE.classList.remove("scrolled");
   }
-}
+});
